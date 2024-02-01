8e943607e97e31a677df1761e6ae88575987df06
Pedroporto87 <pedroporto_8@hotmail.com>
2024-01-31 23:25:36 -0300

    começo de fazer login, porem falta resolver o cookie

--- front-end/src/store/configureStore.js
+++ front-end/src/store/configureStore.js
@@ -1,13 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { workoutReducer } from '../features/getWorkout/getWorkoutSlide'
 import { modalReducer } from '../features/modalSlice'
+import { authReducer } from '../features/authSlice'
+import { apiSlice } from '../features/apiSlice'
 
 const store = configureStore({ 
     reducer: {
         workout: workoutReducer,
-        modal: modalReducer
-    }
-
+        modal: modalReducer,
+        auth: authReducer,
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools: true,
 })
 
 export default store
